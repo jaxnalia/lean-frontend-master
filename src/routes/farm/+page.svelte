5b86d45c3f358bbfb5639b0beabf00ab3bc4d596
jaxnalia <jacksendall@gmail.com>
2024-03-10 15:42:57 +0000

    ui

--- src/routes/farm/+page.svelte
+++ src/routes/farm/+page.svelte
@@ -58,18 +58,12 @@
 		emissionsPerDay = (Number(tokens) * 86400);
 	})
 
-	const glass =
-		'relative flex flex-col items-center backdrop-blur';
 
-	const title =
-		'font-bebas-neue uppercase text-4xl font-black flex flex-col container leading-none';
-
-	const LinkIcon = '<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 2C2.44772 2 2 2.44772 2 3V12C2 12.5523 2.44772 13 3 13H12C12.5523 13 13 12.5523 13 12V8.5C13 8.22386 12.7761 8 12.5 8C12.2239 8 12 8.22386 12 8.5V12H3V3L6.5 3C6.77614 3 7 2.77614 7 2.5C7 2.22386 6.77614 2 6.5 2H3ZM12.8536 2.14645C12.9015 2.19439 12.9377 2.24964 12.9621 2.30861C12.9861 2.36669 12.9996 2.4303 13 2.497L13 2.5V2.50049V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3.70711L6.85355 8.85355C6.65829 9.04882 6.34171 9.04882 6.14645 8.85355C5.95118 8.65829 5.95118 8.34171 6.14645 8.14645L11.2929 3H9.5C9.22386 3 9 2.77614 9 2.5C9 2.22386 9.22386 2 9.5 2H12.4999H12.5C12.5678 2 12.6324 2.01349 12.6914 2.03794C12.7504 2.06234 12.8056 2.09851 12.8536 2.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>';
 </script>
 <GetContractData />
-<div style="background-color: --background;" class={glass}>
+<div style="background-color: --background;" class='relative flex flex-col items-center backdrop-blur'>
 	<div class="h-10" />
-	<h1 class={title}>
+	<h1 class='font-bebas-neue uppercase text-4xl font-black flex flex-col container leading-none'>
 		<span
 			class="text-6xl"
 			style="color: transparent; background-clip: text; -webkit-background-clip: text; background-image: url('https://media1.giphy.com/media/toYOS2FIQKGH1NJ060/giphy.gif');"
@@ -77,14 +71,14 @@
 		>Stake LP Tokens to earn 💜
 	</h1>
 	<div class="h-10 h-10" />
-	<Tabs.Root value="pools" class="w-96 mb-10">
-		<Tabs.List class="grid w-full grid-cols-2 ">
+	<Tabs.Root value="pools" class="w-96 sm:w-4/5 mb-10" style="max-width: 1000px;">
+		<Tabs.List class="grid w-full grid-cols-2" style="max-width: 400px;">
 			<Tabs.Trigger value="pools">Pools</Tabs.Trigger>
 			<Tabs.Trigger value="earn">Earn LEAN 💜</Tabs.Trigger>
 			
 		</Tabs.List>
 		<Tabs.Content value="earn">
-			<Card.Root class="backdrop-blur w-96">
+			<Card.Root class="backdrop-blur w-96 sm:w-auto">
 				<Card.Header class="p-5">
 					<h1 class="text-3xl font-bold">STAKE</h1>
 					<Card.Title><code>LEAN/WPLS LP to earn LEAN 💜</code></Card.Title>
@@ -201,7 +195,7 @@
 			</Card.Root>
 		</Tabs.Content>
 		<Tabs.Content value="pools"> 
-			<Card.Root class="backdrop-blur w-96">
+			<Card.Root class="backdrop-blur w-96 sm:w-auto">
 				<Card.Header class="p-5">
 					<h1 class="text-3xl font-bold">Pools</h1>
 					<Card.Title>Stake PulseX V2 liquidity and earn <strong>LIT</strong></Card.Title>
